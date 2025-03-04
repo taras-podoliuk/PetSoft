c0160ac399e310ab4719a2d1c0582e619ccc66db
Taras Podoliuk <podolyktaras@gmail.com>
2025-03-04 14:31:44 +0200

    removed util from auth edge

--- src/lib/auth-edge.ts
+++ src/lib/auth-edge.ts
@@ -1,5 +1,5 @@
 import { NextAuthConfig } from "next-auth";
-import { getUserByEmail } from "./server-utils";
+import { prisma } from "./db";
 
 export const nextAuthEdgeConfig = {
   pages: {
@@ -58,7 +58,11 @@ export const nextAuthEdgeConfig = {
       if (trigger === "update") {
         // on every request
 
-        const userFromDb = await getUserByEmail(token.email);
+        const userFromDb = await prisma.user.findUnique({
+          where: {
+            email: token.email,
+          },
+        });
         if (userFromDb) {
           token.hasAccess = userFromDb.hasAccess;
         }
